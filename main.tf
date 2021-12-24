46cf61917e72ddb9391b71dad814eed1f82b6f7c
mot <7624397+mototakairaha@users.noreply.github.com>
2021-12-24 16:14:20 +0900

    test

--- main.tf
+++ main.tf
@@ -22,13 +22,13 @@ resource "aws_instance" "example"{
 }
 resource "aws_security_group" "example_ec2" {
     name = "example-ec2"
-    ingress = {
+    ingress  {
         from_port = 80
         to_port = 80
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-    egress = {
+    egress  {
         from_port = 0
         to_port = 0
         protocol = "-1"
