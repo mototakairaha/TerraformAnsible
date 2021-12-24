ba8dcee85e2e195e8679a51065a6ad222aea00dc
mot <7624397+mototakairaha@users.noreply.github.com>
2021-12-24 15:56:08 +0900

    test

--- main.tf
+++ main.tf
@@ -2,9 +2,13 @@ provider "aws" {
     region = "ap-northeast-1"
 }
 
+variable "example_instance_type" {
+    default = "t2.micro"
+}
+
 resource "aws_instance" "example"{
     ami = "ami-0218d08a1f9dac831"
-    instance_type = "t2.micro"
+    instance_type = var.example_instance_type
     tags = {
         Name = "example"
     }
