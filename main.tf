69a6dda92fa9a56992754a94f0a12a02339b9d56
mot <7624397+mototakairaha@users.noreply.github.com>
2021-12-24 16:13:00 +0900

    test

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ variable "example_instance_type" {
 resource "aws_instance" "example"{
     ami = "ami-0218d08a1f9dac831"
     instance_type = var.example_instance_type
+    vpc_security_group_ids = [aws_security_group.example_ec2.id]
     tags = {
         Name = "example"
     }
@@ -18,4 +19,22 @@ resource "aws_instance" "example"{
         systemctl start httpd.service
         systemctl enable httpd.service
     EOF
+}
+resource "aws_security_group" "example_ec2" {
+    name = "example-ec2"
+    ingress = {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress = {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+output "example_instance_id" {
+    value = aws_instance.example.id
 }
